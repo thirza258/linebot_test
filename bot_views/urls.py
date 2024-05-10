464a99f30e1be0852a9028d2f916ed610d6c829c
Thirza <thirzahmad@gmail.com>
2024-05-10 17:00:55 +0700

    adds

--- bot_views/urls.py
+++ bot_views/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # Add your URL patterns here
     # Example: path('home/', views.home, name='home'),
-    
+    path("text_message_user/", views.TextMessageUserViews.as_view()),
 ]
--- linebot_project/urls.py
+++ linebot_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tutorial_bot/", include("tutorial_bot.urls")),
+    path("", include("bot_views.urls")),
 ]
